89da5e7481be345fd9c2d1534ace6e4c2c094526
Harb-A <harbabbas02@gmail.com>
2022-09-18 15:49:14 +0300

    Idle and playable boolean states added

--- game.js
+++ game.js
@@ -5,46 +5,66 @@ you can actually work rather than having to deal with
 */
 window.onload = () => {
 
+
+/*Start maze section*/
+    //On launch, website is idle so you dont get red markers while navigating, game status is on meaning you can play only if you hover "S"
+    let siteState = false;
+    let gameState = true;
+
+    //Grab the "S", if the site is idle and the game is playable make the site active
+    const startBtn = document.getElementById("start");
+    wePlay = () => {
+        if (gameState == true && siteState == false)
+        {
+            siteState = true;
+            console.log(siteState);
+        }
+    }
+    startBtn.addEventListener("mouseover", wePlay);
+
+
+/*Possible losing scenarios section*/
     //Get all the boundary elements and put them in a collection
     //redParentDOM is to avoid the small little box in the bottom from being colored
     const redParentDOM = document.getElementById("game");
     const redZones = redParentDOM.getElementsByClassName('boundary');
 
-    //Create the arrow functions that sets them all to red
+    //When both the site and game are active, if you touch a nono zone it disables both and flags your screen with the message and red colors
     setRed = () => {
-        for (var i = 0 ; i < redZones.length ; i++)
-        {
-            redZones[i].style.backgroundColor = "red";
+        if (siteState == true && gameState == true){
+            siteState = false;
+            gameState = false;
+            console.log(gameState);
+            for (var i = 0 ; i < redZones.length ; i++)
+            {
+                redZones[i].style.backgroundColor = "red";
+            }
+            document.getElementById("status").innerText = "You Lost !"
         }
-        //Need to also display you lost message
-        document.getElementById("status").innerText = "You Lost !"
-
     };
-
-    //Give all the boundaries the function under the event of mouseover
     for (var i = 0 ; i < redZones.length ; i++)
     {
         redZones[i].addEventListener("mouseover", setRed);
     }
 
-    //Grab the start button
-    const startBtn = document.getElementById("start");
 
-    //Arrow function to clean up the losers mess
+/*Reset section*/
+    //You can only click this when you lose, reset the site back to a playable state
     cleanUp = () => {
-        for (var i = 0 ; i < redZones.length ; i++)
-        {
-            redZones[i].style.backgroundColor = "white";
+        if (gameState == false){
+            siteState = true;
+            gameState = true;
+            for (var i = 0 ; i < redZones.length ; i++)
+            {
+                redZones[i].style.backgroundColor = "white";
+            }
+            document.getElementById("status").innerText = "Begin by moving your mouse over the \"S\"";
         }
-        //Reset the starting message too, cant have "you lost" there the entire time it'll make you sad
-        document.getElementById("status").innerText = "Begin by moving your mouse over the \"S\"";
     }
-
-    //Give on click event listener and function
     startBtn.addEventListener("click", cleanUp);
-
-
 }
+
+
 
 
 
